da5cd06d5d650e66f36e2d3e61fd3c13f428766a
Renata <myska2@yandex.ru>
2022-05-24 16:17:50 +0300

    t6.2

---
+++ Task_6.2/Program.cs
@@ -0,0 +1,42 @@
+﻿// найти произведение двух матриц
+
+int[,] matr1 = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+
+int[,] matr2 = {
+    {8, 8, 8},
+    {8, 8, 8},
+    {8, 8, 8}
+};
+
+
+void MultiplicationMatrices(int[,] matr1)
+{
+for (int i = 0; i < 3; i++) 
+{
+    for (int j = 0; j < 3; j++)
+    {
+    matr1[i, j] = matr1[i, j] * matr2[i, j];
+    }
+}
+}
+
+void PrintArray(int[,] matr1)
+{
+    for (int i = 0; i < matr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr1.GetLength(1); j++)
+        {
+        Console.Write($"{matr1[i, j]} ");  
+        }
+        Console.WriteLine();  
+    }
+}
+
+int[,] matrix = new int[3,3];
+MultiplicationMatrices(matr1);
+Console.WriteLine(); 
+PrintArray(matr1);
